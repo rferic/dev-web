e129e57ce9fb61ef8c6f902bf43d5f51b2748db2
Eric RF <eric@playmedic.com>
2017-10-16 15:23:10 +0200

    add route /profile

--- routes/web.php
+++ routes/web.php
@@ -83,5 +83,10 @@ Route::group(
     	{
             return view('welcome');
     	});
+        
+        Route::get('/profile', function()
+    	{
+            return view('profile');
+    	});
     }
 );
