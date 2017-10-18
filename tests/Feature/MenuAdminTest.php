32181801c62163084eaa3f18f161f5d51be4b933
Eric RF <eric@playmedic.com>
2017-10-18 17:02:40 +0200

    dev testing admin menu

--- tests/Feature/MenuAdminTest.php
+++ tests/Feature/MenuAdminTest.php
@@ -16,7 +16,7 @@ class MenuAdminTest extends TestCase
 {
     use DatabaseMigrations;
     
-    protected $user, $menu, $items;
+    protected $user, $menu, $items, $urlOriginFake = '/fake';
     
     protected function setUp ()
     {
@@ -62,5 +62,86 @@ class MenuAdminTest extends TestCase
                 ->assertStatus(200)
                 ->assertSee($this->menu->name)
                 ->assertSee($this->menu->description);
+    }
+    
+    public function testAddMenu ()
+    {
+        $this->withExceptionHandling();
+        
+        $menu = factory(Menu::class)->make();
+        
+        /******* ERRORS *******/
+        $response = $this
+                ->actingAs($this->user)
+                ->post(route('admin.menu.store'), []);
+        
+        $response
+                ->assertStatus(302)
+                ->assertSessionHasErrors('name')
+                ->assertSessionHasErrors('description');
+        
+        /******* SUCCESS *******/
+        $response = $this
+                ->actingAs($this->user)
+                ->post(route('admin.menu.store'), $menu->toArray());
+        
+        $response
+                ->assertStatus(302)
+                ->assertSessionHas('message', __('Menu has been created'));
+    }
+    
+    public function testUpdateMenu ()
+    {
+        $this->withExceptionHandling();
+        
+        $menu = factory(Menu::class)->make();
+        
+        /******* ERRORS *******/
+        $response = $this
+                ->actingAs($this->user)
+                ->put(route('admin.menu.edit', $this->menu->id));
+        
+        $response
+                ->assertStatus(302)
+                ->assertSessionHasErrors('name')
+                ->assertSessionHasErrors('description');
+        
+        /******* SUCCESS *******/
+        $response = $this
+                ->actingAs($this->user)
+                ->put(route('admin.menu.edit', $this->menu->id), $menu->toArray());
+        
+        $response
+                ->assertStatus(302)
+                ->assertRedirect(route('admin.menu.detail', $this->menu->id))
+                ->assertSessionHas('message', ['class' => 'alert-success', 'content' => __('Menu has been updated')]);
+        
+    }
+    
+    public function testTrashMenu ()
+    {
+        $this->withExceptionHandling();
+        
+        $response = $this
+                ->actingAs($this->user)
+                ->get(route('admin.menu.trash', $this->menu->id), ['HTTP_REFERER' => $this->urlOriginFake]);
+        
+        $response
+                ->assertStatus(302)
+                ->assertRedirect($this->urlOriginFake)
+                ->assertSessionHas('message', ['class' => 'alert-success', 'content' => __('Menu has been moved to trash')])
+                ->assertSessionHas('currentPanel', 'trash');
+        
+    }
+    
+    public function testRestoreMenu ()
+    {
+        $this->withExceptionHandling();
+        
+    }
+    
+    public function testDestroyMenu ()
+    {
+        $this->withExceptionHandling();
     }
 }
