49c4c9d549abfe43eb14cdc7665372d2d6087b68
Eric RF <eric@playmedic.com>
2017-10-23 17:14:17 +0200

    dev admin page

--- resources/assets/js/admin/components/Page.vue
+++ resources/assets/js/admin/components/Page.vue
@@ -30,7 +30,33 @@
       }
     },
     mounted () {
-      this.pagesLocales = JSON.parse(this.page_locales_json)
+      let pagesOrigin = JSON.parse(this.page_locales_json)
+      let page = null
+      let context = this
+      
+      Object.keys(context.supportedLocales).forEach(function(key, supportedLocale) {
+        pagesOrigin.forEach((pageOrigin) => {
+          if (pageOrigin.lang === key) {
+            page = pageOrigin
+            return false
+          }
+        })
+        
+        context.pagesLocales.push({
+          lang: supportedLocale,
+          lang_iso: key,
+          status: (page === null) ? false : true,
+          id: (page === null) ? null : page.id,
+          slug: (page === null) ? '' : page.slug,
+          title: (page === null) ? '' : page.title,
+          description: (page === null) ? '' : page.description,
+          layout: (page === null) ? '' : page.layout,
+          options: (page === null) ? {} : JSON.parse(page.options),
+          seo_title: (page === null) ? '' : page.seo_title,
+          seo_description: (page === null) ? '' : page.seo_description,
+          seo_keywords: (page === null) ? [] : JSON.parse(page.seo_keywords)
+        })
+      })
     },
     methods: {}
   }
