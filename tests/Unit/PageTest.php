3536745efea67d1470f09a049712080fa7392dbc
Eric RF <eric@playmedic.com>
2018-02-19 16:54:49 +0100

    dev page test

--- tests/Unit/PageTest.php
+++ tests/Unit/PageTest.php
@@ -6,12 +6,22 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 use Illuminate\Database\Eloquent\Collection;
 
+use Spatie\Permission\Models\Role;
+
+use App\User;
+
 class PageTest extends TestCase
 {
     use DatabaseMigrations;
     
     protected function setUp ()
     {
+        parent::setUp();
         
+        app()['cache']->forget('spatie.permission.cache');
+
+        Role::create(['name' => 'admin']);
+        
+        $this->user = factory(User::class)->create()->assignRole('admin');
     }
 }
