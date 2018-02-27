f8e0e1c9dd6f03ecc9e6ee154cc10a86739b8d0a
Eric RF <eric@playmedic.com>
2018-02-27 15:20:53 +0100

    dev page test

--- tests/Unit/PageTest.php
+++ tests/Unit/PageTest.php
@@ -11,12 +11,15 @@ use Spatie\Permission\Models\Role;
 use App\User;
 use App\Page;
 use App\PageLocale;
+use App\Content;
+use App\Menu;
+use App\MenuItem;
 
 class PageTest extends TestCase
 {
     use DatabaseMigrations;
     
-    protected $user, $page, $locales;
+    protected $user, $page, $locales, $menu;
     
     protected function setUp ()
     {
@@ -27,18 +30,40 @@ class PageTest extends TestCase
         Role::create(['name' => 'admin']);
         
         $this->user = factory(User::class)->create()->assignRole('admin');
-        $this->page = factory(Page::class)->create([
-            'user_id' => $this->user->id
-        ]);
+        $this->page = factory(Page::class)->create([ 'user_id' => $this->user->id ]);
+        $this->menu = factory(Menu::class, 1)->create([ 'user_id' => $this->user->id ]);
         $this->locales = [
+            
             factory(PageLocale::class, 1)->create([
                 'lang' => 'en',
                 'page_id' => $this->page->id 
-            ]),
+            ])->each(function ($locale) {
+                factory(Content::class, 2)->create([
+                    'page_locale_id' => $locale->id
+                ]);
+                
+                factory(MenuItem::class, 1)->create([
+                    'type' => 'internal',
+                    'page_locale_id' => $locale->id,
+                    'lang' => $locale->lang
+                ]);
+            }),
+                    
             factory(PageLocale::class, 1)->create([
                 'lang' => 'es',
                 'page_id' => $this->page->id 
-            ])
+            ])->each(function ($locale) {
+                factory(Content::class, 2)->create([
+                    'page_locale_id' => $locale->id
+                ]);
+                
+                factory(MenuItem::class, 1)->create([
+                    'type' => 'internal',
+                    'page_locale_id' => $locale->id,
+                    'lang' => $locale->lang
+                ]);
+            })
+            
         ];
     }
     
@@ -53,9 +78,24 @@ class PageTest extends TestCase
     {
         $this->assertInstanceOf(User::class, $this->page->author);
     }
-
-    public function isAuthor ()
+    
+    public function testPageIsAuthor ()
     {
-        return $this->owner->id === auth()->id();
+        $this->assertFalse($this->page->isAuthor());
+        $this->signIn($this->user)->assertTrue($this->page->isAuthor());
+    }
+    
+    public function testPageHasThoughtContent ()
+    {
+        $this->assertCount(4, $this->page->contents);
+        $this->assertInstanceOf(Collection::class, $this->page->contents);
+        $this->assertInstanceOf(Content::class, $this->page->contents->first());
+    }
+    
+    public function testPageHasThoughtMenuItems ()
+    {
+        $this->assertCount(2, $this->page->menuItems);
+        $this->assertInstanceOf(Collection::class, $this->page->menuItems);
+        $this->assertInstanceOf(MenuItem::class, $this->page->menuItems->first());
     }
 }
