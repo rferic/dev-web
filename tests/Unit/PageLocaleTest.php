9b2e14c4534939dcb56cb1f6e2dc2f0be9c34f69
Eric RF <eric@playmedic.com>
2018-03-02 09:31:38 +0100

    dev pagelocale test

--- tests/Unit/PageLocaleTest.php
+++ tests/Unit/PageLocaleTest.php
@@ -6,7 +6,31 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 use Illuminate\Database\Eloquent\Collection;
 
+use Spatie\Permission\Models\Role;
+
+use App\User;
+use App\Page;
+use App\PageLocale;
+
 class PageLocaleTest extends TestCase
 {
     use DatabaseMigrations;
+    
+    protected $user, $page, $locale;
+    
+    protected function setUp ()
+    {
+        parent::setUp();
+        
+        app()['cache']->forget('spatie.permission.cache');
+
+        Role::create(['name' => 'admin']);
+        
+        $this->user = factory(User::class)->create()->assignRole('admin');
+        $this->page = factory(Page::class)->create([ 'user_id' => $this->user->id ]);
+        $this->locale = factory(PageLocale::class)->create([
+            'lang' => 'en',
+            'page_id' => $this->page->id 
+        ]);
+    }
 }
